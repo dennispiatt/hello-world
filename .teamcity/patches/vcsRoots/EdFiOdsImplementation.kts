595bd79ab7920d00e7c6b2d35586ebc3ed1b89bd
d.piatt <d.piatt@TeamCity>
2022-04-06 14:14:09 +0000

    TeamCity change in 'mn-mde-edfi' project: 'Ed-Fi-ODS-Implementation' VCS root was updated

--- .teamcity/patches/vcsRoots/EdFiOdsImplementation.kts
+++ .teamcity/patches/vcsRoots/EdFiOdsImplementation.kts
@@ -13,7 +13,8 @@ create(DslContext.projectId, GitVcsRoot({
     id("EdFiOdsImplementation")
     name = "Ed-Fi-ODS-Implementation"
     url = "https://github.com/mn-mde-edfi/Ed-Fi-ODS-Implementation"
-    branch = "refs/heads/develop-MN-v31"
+    branch = "refs/heads/main-v52"
+    branchSpec = "+:*"
     authMethod = password {
         userName = "dennispiatt"
         password = "credentialsJSON:0f12473a-0eb1-4d19-b397-866c46491400"
