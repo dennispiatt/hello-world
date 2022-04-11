ce43f342beac5427a97d96b169d4a601e29bb91a
d.piatt <d.piatt@TeamCity>
2022-04-11 14:40:31 +0000

    TeamCity change in 'mn-mde-edfi' project: runners of '2. Build and push Docker images' build configuration were updated

--- .teamcity/patches/buildTypes/BuildAndPushDockerContainers.kts
+++ .teamcity/patches/buildTypes/BuildAndPushDockerContainers.kts
@@ -2,6 +2,7 @@ package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.BuildStep
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.PowerShellStep
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.powerShell
 import jetbrains.buildServer.configs.kotlin.v2019_2.ui.*
 
@@ -212,6 +213,68 @@ changeBuildType(RelativeId("BuildAndPushDockerContainers")) {
         }
     }
     steps {
+        update<PowerShellStep>(1) {
+            clearConditions()
+            scriptMode = script {
+                content = """
+                    ${'$'}ErrorActionPreference = "Stop"
+                    ${'$'}repo = "nexus"
+                    ${'$'}imageName = "edfi-docker"
+                    ${'$'}schoolYear = "%school.year%"
+                    ${'$'}buildCounter = "%build.counter%"
+                    
+                    Write-Host "docker build admin app"
+                    ${'$'}imageName = "ods-api-web-admin-app"
+                    
+                    ${'$'}User = "%nexus.nuget.username%"
+                    ${'$'}PWord = ConvertTo-SecureString -String "%nexus.nuget.password%" -AsPlainText -Force
+                    ${'$'}Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ${'$'}User, ${'$'}PWord
+                    ${'$'}url = "%mn-mde-edfi.nexus.host%/repository/edfi-raw/ssl.zip"
+                    
+                    Invoke-WebRequest -Uri ${'$'}url -Credential ${'$'}Credential -AllowUnencryptedAuthentication -OutFile ./Web-Ods-AdminApp/Alpine/mssql/ssl.zip  
+                    
+                    docker build -t ${'$'}imageName ./Web-Ods-AdminApp/Alpine/mssql
+                    if (${'$'}error.count -gt 0 -or ${'$'}LASTEXITCODE -gt 0) { exit 1; }
+                    docker tag ${'$'}imageName ${'$'}repo/${'$'}(${'$'}imageName).%school.year%:%build.counter%
+                    if (${'$'}error.count -gt 0 -or ${'$'}LASTEXITCODE -gt 0) { exit 1; }
+                    docker tag ${'$'}imageName ${'$'}repo/${'$'}(${'$'}imageName):latest
+                    if (${'$'}error.count -gt 0 -or ${'$'}LASTEXITCODE -gt 0) { exit 1; }
+                    docker save ${'$'}repo/${'$'}(${'$'}imageName):latest -o "${'$'}(${'$'}imageName).tar"
+                    
+                    Write-Host "docker build ods web api"
+                    ${'$'}imageName = "ods-api-web-api"
+                    docker build -t ${'$'}imageName ./Web-Ods-Api/Alpine/mssql
+                    if (${'$'}error.count -gt 0 -or ${'$'}LASTEXITCODE -gt 0) { exit 1; }
+                    docker tag ${'$'}imageName ${'$'}repo/${'$'}(${'$'}imageName).%school.year%:%build.counter%
+                    if (${'$'}error.count -gt 0 -or ${'$'}LASTEXITCODE -gt 0) { exit 1; }
+                    docker tag ${'$'}imageName ${'$'}repo/${'$'}(${'$'}imageName):latest
+                    if (${'$'}error.count -gt 0 -or ${'$'}LASTEXITCODE -gt 0) { exit 1; }
+                    docker save ${'$'}repo/${'$'}(${'$'}imageName):latest -o "${'$'}(${'$'}imageName).tar"
+                    
+                    Write-Host "docker build sandbox admin"
+                    ${'$'}imageName = "ods-api-web-sandbox-admin"
+                    docker build -t ${'$'}imageName ./Web-Sandbox-Admin/Alpine/mssql
+                    if (${'$'}error.count -gt 0 -or ${'$'}LASTEXITCODE -gt 0) { exit 1; }
+                    docker tag ${'$'}imageName ${'$'}repo/${'$'}(${'$'}imageName).%school.year%:%build.counter%
+                    if (${'$'}error.count -gt 0 -or ${'$'}LASTEXITCODE -gt 0) { exit 1; }
+                    docker tag ${'$'}imageName ${'$'}repo/${'$'}(${'$'}imageName):latest
+                    if (${'$'}error.count -gt 0 -or ${'$'}LASTEXITCODE -gt 0) { exit 1; }
+                    docker save ${'$'}repo/${'$'}(${'$'}imageName):latest -o "${'$'}(${'$'}imageName).tar"
+                    
+                    Write-Host "docker build swagger"
+                    ${'$'}imageName = "ods-api-web-swagger-ui"
+                    docker build -t ${'$'}imageName ./Web-SwaggerUI/Alpine
+                    if (${'$'}error.count -gt 0 -or ${'$'}LASTEXITCODE -gt 0) { exit 1; }
+                    docker tag ${'$'}imageName ${'$'}repo/${'$'}(${'$'}imageName).%school.year%:%build.counter%
+                    if (${'$'}error.count -gt 0 -or ${'$'}LASTEXITCODE -gt 0) { exit 1; }
+                    docker tag ${'$'}imageName ${'$'}repo/${'$'}(${'$'}imageName):latest
+                    if (${'$'}error.count -gt 0 -or ${'$'}LASTEXITCODE -gt 0) { exit 1; }
+                    docker save ${'$'}repo/${'$'}(${'$'}imageName):latest -o "${'$'}(${'$'}imageName).tar"
+                    
+                    ls *.tar | Out-Host
+                """.trimIndent()
+            }
+        }
         update<BuildStep>(5) {
             clearConditions()
             param("secure:octopus_apikey", "credentialsJSON:e6aacf31-740c-42fb-831f-2257142b455a")
